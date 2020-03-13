af0921970e7a17e4bb25f5bdd80438893e1de616
= <you@example.com>
2020-03-13 15:38:15 +0100

    adding_01_01_Epsilon

---
+++ 01_01_Epsilon/01_01_Epsilon/Calculations.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_01_Epsilon
+{
+    public static class Calculations
+    {
+        public enum ValidShapes
+        {
+            Square, Circle, Cylinder, Octagon
+        }
+
+        [Flags]
+        public enum ValidColors
+        {
+            Red = 0, Green = 1, Blue = 2
+        }
+
+        public static void ConvertFromIntToShort(int sourceValue, out short destinationValue)
+        {
+            destinationValue = 0;
+
+            if(sourceValue <= short.MaxValue && sourceValue >= short.MinValue)
+            {
+                destinationValue = (short)sourceValue;
+                Console.WriteLine($"Convert From Int To Short Success! {destinationValue}");
+            }
+            else
+            {
+                Console.WriteLine("Convert From Int To Short Fail!");
+            }
+        }
+
+        public static double DegreeToRadConversion(double degree)
+        {
+            return (Math.PI / 180) * degree;
+        }
+
+        public static int GetLSW(int number)
+        {
+            int setOfLswSixteenBits = (int)Math.Pow(2, 16) - 1;
+            return number & setOfLswSixteenBits;
+        }
+
+        public static int GetMFW(int number)
+        {
+            int setOfMswSixteenBits = (int)(Math.Pow(2, 16) - 1) << 16;
+            return number & setOfMswSixteenBits;
+        }
+
+        public static bool IsApproximatelyEqualTo(double enumerator, double denominator, double dbValue)
+        {
+            return IsApproximatelyEqualTo(enumerator, denominator, dbValue, double.Epsilon);
+        }
+
+        public static bool IsApproximatelyEqualTo(double enumerator, double denominator, double dbValue,
+            double epsilon)
+        {
+            double difference = (enumerator / denominator) - dbValue;
+
+            if (Math.Abs(difference) < epsilon)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsEven(int number)
+        {
+            return (number % 2) == 0;
+        }
+
+        public static bool IsOdd(int number)
+        {
+            return (number % 2) != 0;
+        }
+
+        public static byte NegateNumber(byte num)
+        {
+            return (byte)~num;
+        }
+
+        public static void TryToParseStringToDouble(string word, out double num)
+        {
+            if (double.TryParse(word, out num))
+            {
+                Console.WriteLine($"Parse success: {num}");
+            }
+            else
+            {
+                Console.WriteLine("Parse Fail");
+            }
+        }
+
+        public static double RadToDegreeConversion(double radians)
+        {
+            return (180 / Math.PI) * radians;
+        }
+    }
+}
---
+++ 01_01_Epsilon/01_01_Epsilon/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _01_01_Epsilon.Calculations;
+
+namespace _01_01_Epsilon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n 1.1. Comparing fractions: \n\n");
+
+            Console.WriteLine($"Epsilon value: {double.Epsilon}");
+
+            double enumerator = 1;
+            double denominator = 126;
+            double dbValue = 0.007936507;
+            double epsilon = 0.0001;
+
+            Console.WriteLine($"Is equal to : " + 
+                $"{IsApproximatelyEqualTo(enumerator, denominator, dbValue, epsilon)}");
+
+
+            Console.WriteLine("\n\n 1.2. 1.3 Degree to rad and inverse conversion:\n");
+
+            double degree = 120;
+            double radians = 2;
+
+            Console.WriteLine($"Deg: {degree} - Rad: {DegreeToRadConversion(degree)}");
+            Console.WriteLine($"Deg: {radians} - Rad: {RadToDegreeConversion(radians)}");
+
+
+            Console.WriteLine("\n\n 1.4. Bit negation:\n");
+
+            byte byteNumber = 0;
+            Console.WriteLine($"number: {byteNumber} - negation: {NegateNumber(byteNumber)}");
+
+
+            Console.WriteLine("\n\n 1.5. Even/Odd numbers check \n");
+
+            int numberEven = 2;
+            int numberOdd = 3;
+
+            Console.WriteLine($"Number: {numberEven} is even: {IsEven(numberEven)}");
+            Console.WriteLine($"Number: {numberOdd} is even: {IsOdd(numberOdd)}");
+
+
+            Console.WriteLine("\n\n 1.6. Obtaining LSW, MSW from 32 bit number \n");
+
+            int numberToGetMswAndLsw = (int)Math.Pow(2, 17) + (int)Math.Pow(2, 16) + 3;
+
+            Console.WriteLine($"Number: {numberToGetMswAndLsw} - MSW: {GetMFW(numberToGetMswAndLsw)}," +
+                $" LSW: {GetLSW(numberToGetMswAndLsw)}");
+
+
+            Console.WriteLine("\n\n 1.7. Conversion from numbers of different systems \n");
+
+            int hexB = 0xFA;
+            string hexBase = "FA";
+            string octBase = "27";
+            string binBase = "11111111";
+
+            Console.WriteLine($"Num {hexBase} - Dec: {Convert.ToInt32(hexBase, 16)}");
+            Console.WriteLine($"Num {hexBase} - Dec: {Convert.ToInt32(octBase, 8)}");
+            Console.WriteLine($"Num {hexBase} - Dec: {Convert.ToInt32(binBase, 2)}");
+            Console.WriteLine($"No conversion required: {hexB}");
+
+            Console.WriteLine("\n\n 1.8. Checking reprosentation of a string \n");
+
+            string strToCheck = "12,5";
+            double numberToWrite;
+
+            TryToParseStringToDouble(strToCheck, out numberToWrite);
+
+            Console.WriteLine("\n\n 1.9. - 1.10 Round a number \n");
+
+            Console.WriteLine($" Round a number: " + 
+                $"\n\tRound of 2.9 equals {Math.Round(2.9)}" +
+                $"\n\tCeiling of 2.3 equals {Math.Ceiling(2.3)}" + 
+                $"\n\tFloor of 2.8 equals {Math.Floor(2.8)}");
+
+            Console.WriteLine("\n\n 1.11. - 1.12 Changing Celcius to Fahrenheit and reversely - skipped \n");
+
+            Console.WriteLine("\n\n 1.13. - Safe conversion \n");
+
+            int sourceValue = 3400;
+            short destinationValue;
+
+            ConvertFromIntToShort(sourceValue, out destinationValue);
+
+            Console.WriteLine("\n\n 1.14. - 1.15 Triangle calculations - skipped \n");
+
+            Console.WriteLine("\n\n1.16 - enums\n");
+
+            ValidShapes shape = ValidShapes.Circle;
+
+            Console.WriteLine($"shape decimal equivalent: {shape.ToString("D")}");
+            Console.WriteLine($"shape general representation: {ValidShapes.Cylinder.ToString("G")}");
+            Console.WriteLine($"shape hex representation: {shape.ToString("X")}");
+
+            ValidColors validColor = ValidColors.Blue | ValidColors.Green;
+
+            Console.WriteLine($"\nColors flags attributes usage Blue OR Green: {validColor}");
+            Console.WriteLine($"Colors flags attribute usage Blue OR Green: {validColor.ToString("D")}");
+
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
